214b2fad4f1434de8190b252c9740346c0bc9aa5
luukkonen1991 <luukkonen1991@gmail.com>
2019-12-19 14:37:51 +0200

    login backround changed and trying to work with aws build

---
+++ trying/server - build/public/assets/js/game.js
@@ -0,0 +1,30 @@
+var config = {
+    type: Phaser.WEBGL,
+    width: 480,
+    height: 640,
+    backgroundColor: "black",
+    physics: {
+        default: "arcade",
+        arcade: {
+            gravity: { x: 0, y: 0 }
+        }
+    },
+    scene: [
+        SceneMainMenu,
+        SceneStory,
+        SceneMain,
+        SceneStageCleared,
+        SceneGameOver
+    ],
+    pixelArt: true,
+    roundPixels: true,
+};
+
+var game = new Phaser.Game(config);
+var score = 0;
+var allData;
+
+var ammoText = '';
+var launched = true;
+var bosslaunched = true;
+var timer = 500;
